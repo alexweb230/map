8aaf2553d06cae2365abe6e8c797cebf33fe5ffd
tania <tani.zhelezniak@gmail.com>
2018-11-15 16:09:08 +0200

    map

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,4 @@
-
-$(document).ready(function(){
+$(document).ready(function () {
 
     $(".path").hover(
         function () {
@@ -12,13 +11,36 @@ $(document).ready(function(){
         }
     )
     $(".path").mousemove(
-        function(pos) {
+        function (pos) {
             $("#descr")
-                .css('left',(pos.pageX+10)+'px')
-                .css('top',(pos.pageY+10)+'px');
+                .css('left', (pos.pageX + 10) + 'px')
+                .css('top', (pos.pageY + 10) + 'px');
         }
     );
 
 
-
 });
+// Load google charts
+
+let first = "П.Порошенко",
+    second = "Ю.Тимошенко",
+    three = "В.Рабинович";
+google.charts.load('current', {'packages': ['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    // Define the chart to be drawn.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Element');
+    data.addColumn('number', 'Percentage');
+    data.addRows(
+        [
+            [first, 60],
+            [second, 20],
+            [three, 20]
+        ]);
+    var options = {'width': 600, 'height': 500};
+    // Instantiate and draw the chart.
+    var chart = new google.visualization.PieChart(document.getElementById('myPieChart'));
+    chart.draw(data, options);
+}

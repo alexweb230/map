c0bbc699b7c553739ca8823471c15e786e5087b6
tania <tani.zhelezniak@gmail.com>
2018-11-30 13:27:27 +0200

    last

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,26 +1,3 @@
-// $(document).ready(function () {
-//     $(".path").hover(
-//         function () {
-//             let data = $(this).attr('data-region');
-//             let dataCount= $(this).attr('data-count-1');
-//             console.log(dataCount);
-//             $('#descr').show();
-//             $('#descr .h4').html(data);
-//             $('.precent').html(`${dataCount}%`);
-//             $('.progress .progress-item').css("width", dataCount + "%");
-//         },
-//         function () {
-//             $('#descr').hide();
-//         }
-//     );
-//     $(".path").mousemove(
-//         function (pos) {
-//             $("#descr")
-//                 .css('left', (pos.pageX + 10) + 'px')
-//                 .css('top', (pos.pageY + 10) + 'px');
-//         }
-//     );
-// });
 
 
 window.onload = function () {
